d11ea4704a61f48a001f522c2879eef54244ddac
Alex Kaszynski <akascap@gmail.com>
2022-12-09 11:05:48 +0000

    Update ansys/tools/protoc_helper/__init__.py

--- ansys/tools/protoc_helper/__init__.py
+++ ansys/tools/protoc_helper/__init__.py
@@ -2,5 +2,5 @@
 from ._compile_protos import *
 from ._distutils_overrides import *
 
-__version__ = "0.1.2"
+__version__ = "0.2.dev0"
 __all__ = _compile_protos.__all__ + _distutils_overrides.__all__  # type: ignore
