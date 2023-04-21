e970360e6ce62bed3362321ffd0fe46f71123c1d
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-04-21 16:30:12 +0200

    DOCS: update import statement

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,7 +1,7 @@
 """Sphinx documentation configuration file."""
 from datetime import datetime
 
-from pyansys_sphinx_theme import pyansys_logo_black
+from ansys_sphinx_theme import pyansys_logo_black
 
 from ansys.tools.protoc_helper import __version__
 
