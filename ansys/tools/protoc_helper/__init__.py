ce42a005322eb2100820cdb0b66664f8315ae6a6
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2026-02-09 16:58:27 +0100

    release: v0.6.0

--- ansys/tools/protoc_helper/__init__.py
+++ ansys/tools/protoc_helper/__init__.py
@@ -2,5 +2,5 @@
 from ._compile_protos import *
 from ._distutils_overrides import *
 
-__version__ = "0.6.dev0"
+__version__ = "0.6.0"
 __all__ = _compile_protos.__all__ + _distutils_overrides.__all__  # type: ignore
